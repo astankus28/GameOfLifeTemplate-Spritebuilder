87458129085565f8b6f1bcf2d6c3bd0bb9d9894b
astankus28 <abriani@abrianis-imac.neo.rr.com>
2014-06-30 14:36:57 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -57,28 +57,12 @@ static const int GRID_COLUMNS = 10;
             _gridArray[i][j] = creature;
             
             
+            
             x+=_cellWidth;
         }
         
         y += _cellHeight;
     }
-}
-
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
-{
-    //get the x,y coordinates of the touch
-    CGPoint touchLocation = [touch locationInNode:self];
-    
-    //get the Creature at that location
-    Creature *creature = [self creatureForTouchPosition:touchLocation];
-    
-    //invert it's state - kill it if it's alive, bring it to life if it's dead.
-    creature.isAlive = !creature.isAlive;
-}
-
-- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
-{
-    //get the row and column that was touched, return the Creature inside the corresponding cell
 }
 
 @end
