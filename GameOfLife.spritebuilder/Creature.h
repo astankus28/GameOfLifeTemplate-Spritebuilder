7f572ec2cc6e76ded27ee0fa89d8e3821c2c9a44
astankus28 <abriani@abrianis-imac.neo.rr.com>
2014-06-30 14:18:37 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.h
@@ -0,0 +1,21 @@
+//
+//  Creatures.h
+//  GameOfLife
+//
+//  Created by Abriani on 6/30/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+- (id)initCreature;
+
+@end
